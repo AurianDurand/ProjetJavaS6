d9f5aefed53f670b46ded7c03b1e8572007fff76
Bugnone Michaël <bugnone.michael@gmail.com>
2020-03-15 11:42:41 +0100

    Res update

---
+++ Res/Maps/Object.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Object" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../Textures/entity_64x64.png" width="512" height="512"/>
+ <tile id="32">
+  <properties>
+   <property name="spawn_red_ghost" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
