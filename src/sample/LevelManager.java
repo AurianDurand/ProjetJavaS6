37b2461bf7b8bf764997e2cb82c54150f11ce524
Aurian Durand <durand.aurian@me.com>
2020-03-18 18:19:25 +0100

    Init feature/parse-map

--- src/sample/LevelManager.java
+++ src/sample/LevelManager.java
@@ -1,4 +1,31 @@
 package sample;
 
+import sample.entityManager.EntityManager;
+import sample.map.Map;
+
 public class LevelManager {
+
+    private Map map;
+    private EntityManager entityManager= new EntityManager();
+    private String mapName;
+
+    public LevelManager(String mapName) {
+        this.mapName = mapName;
+        this.callParser();
+    }
+
+    /**
+     * this method call the parser and implements the map
+     * and entities (objects and dynamic entities)
+     */
+    private void callParser() {
+
+        // args
+        
+
+
+        // instantiate the parser here
+
+
+    }
 }
