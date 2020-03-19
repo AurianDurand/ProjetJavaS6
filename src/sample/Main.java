4b0727681768ac9ed4e33afb2f765d30ff9fb026
Aurian Durand <durand.aurian@me.com>
2020-03-19 19:00:08 +0100

    Menu v1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,34 +2,47 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import sample.cours1.ModelePacmanSimple;
-import sample.cours1.VTerminal;
-import sample.map.Map;
+import sample.view.HomePage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        int width = 1536; // 1920 * 0.8
+        int height = 864; // 1080 * 0.8
+
+        primaryStage.setTitle("Pacman");
+        Group root = new Group();
+
+        HomePage homePage = new HomePage(primaryStage, width, height);
+        root.getChildren().add(homePage);
+
+        primaryStage.setScene(new Scene(root, width, height));
+        primaryStage.show();
+
+        // default code
+        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.show();*/
     }
 
 
     public static void main(String[] args) {
-        // launch(args);
+        launch(args);
 
-        ModelePacmanSimple modele = new ModelePacmanSimple();
+        /*ModelePacmanSimple modele = new ModelePacmanSimple();
         VTerminal vue = new VTerminal(modele);
         modele.addObserver(vue);
-        new Thread(modele).start();
+        new Thread(modele).start();*/
 
         // test to verify if the map works properly
-        Map map = new Map(10,10);
+        //Map map = new Map(10,10);
 
     }
 }
